cdb2f132e2347c7b85f1455c6405fbcbba9e72ff
maxwellwachira <maxwellwachira67@gmail.com>
2022-09-29 18:30:45 +0300

    added mantine theming

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,42 @@
-import '../styles/globals.css'
+import { useEffect, useState } from 'react';
 import type { AppProps } from 'next/app'
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core';
+import { useColorScheme, useHotkeys, useLocalStorage  } from '@mantine/hooks';
+
+import PageLoader from '../components/pageLoader/pageLoader';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [loading, setLoading] = useState(false);
+
+  const preferredColorScheme = useColorScheme();
+
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'mantine-color-scheme',
+    defaultValue: preferredColorScheme,
+    getInitialValueInEffect: true,
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  useHotkeys([['mod+J', () => toggleColorScheme()]]);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(true), 2000);
+  }, []);
+
+  return (
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+        { !loading ?
+          (<PageLoader />) :
+          (<Component {...pageProps} />)
+        }
+        
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
+  
 }
 
-export default MyApp
+export default MyApp;
