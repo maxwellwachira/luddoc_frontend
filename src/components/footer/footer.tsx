beb77181b02a7045cad816dfd11e812e80b29da8
maxwellwachira <maxwellwachira67@gmail.com>
2022-09-30 17:18:11 +0300

    added luddoc logo on footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import { IconBrandTwitter, IconBrandYoutube, IconBrandInstagram } from '@tabler/
 import Image from 'next/image';
 
 import { useStyles } from './footer.styles';
-
+import logo from '../../assets/logo.png';
 
 
 interface FooterLinksProps {
@@ -42,20 +42,20 @@ interface FooterLinksProps {
       <Container className={classes.inner}>
         <div className={classes.logo}>
           <Image 
-            src="/logo.svg"
-            width={100}
-            height={50}
+            src={logo}
+            width={130}
+            height={130}
             alt='logo'
           />
           <Text size="xs" color="dimmed" className={classes.description}>
-            Build fully functional accessible web applications faster than ever
+            Don't wait, enroll today
           </Text>
         </div>
         <div className={classes.groups}>{groups}</div>
       </Container>
       <Container className={classes.afterFooter}>
         <Text color="dimmed" size="sm">
-          © 2022 . All rights reserved.
+          © 2022 Luddoc S.F.L. All rights reserved.
         </Text>
 
         <Group spacing={0} className={classes.social} position="right" noWrap>
