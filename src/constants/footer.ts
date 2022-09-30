f29f4be6a5af553edd18b40f4ec9fa082857b32b
maxwellwachira <maxwellwachira67@gmail.com>
2022-09-30 17:15:24 +0300

    footer links

---
+++ src/constants/footer.ts
@@ -0,0 +1,38 @@
+export const footerData = [
+    {
+        title: 'Site Map', 
+        links: [
+            { id: 0, link: 'https://xx.com', label: 'Home' },
+            { id: 1, link: 'https://xx.com', label: 'About' },
+            { id: 2, link: 'https://xx.com', label: 'Courses' },
+            { id: 3, link: 'https://xx.com', label: 'FAQ' }
+        ]
+    },
+    {
+        title: 'Courses', 
+        links: [
+            { id: 0, link: 'https://xx.com', label: 'Taking Care of Elderly' },
+            { id: 1, link: 'https://xx.com', label: 'First Aid' },
+            { id: 2, link: 'https://xx.com', label: 'Self Development' },
+            { id: 3, link: 'https://xx.com', label: 'Lending' }
+        ]
+    },
+    {
+        title: 'Contact', 
+        links: [
+            { id: 0, link: 'https://xx.com', label: 'Contact Page' },
+            { id: 1, link: 'https://xx.com', label: 'Support' },
+            { id: 2, link: 'https://xx.com', label: 'Info' },
+            { id: 2, link: 'https://xx.com', label: 'Sales' },
+        ]
+    }, 
+    {
+        title: 'Social Links', 
+        links: [
+            { id: 0, link: 'https://xx.com', label: 'Twitter' },
+            { id: 1, link: 'https://xx.com', label: 'LinkedIn' },
+            { id: 2, link: 'https://xx.com', label: 'Instagram' },
+            { id: 3, link: 'https://xx.com', label: 'Facebook' }
+        ]
+    },
+];
