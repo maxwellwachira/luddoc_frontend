6c90b34c9d602c819a8a3201223f183d91b3f5b2
maxwellwachira <maxwellwachira67@gmail.com>
2022-10-01 12:03:38 +0300

    Added resetpassword component to index for ease of export

--- src/features/authentication/index.ts
+++ src/features/authentication/index.ts
@@ -1,5 +1,6 @@
 import Login from "./components/login/login";
 import Register from "./components/register/register";
 import ForgotPassword from "./components/forgotPassword/forgotPassword";
+import ResetPassword from "./components/resetPassword/resetPassword";
 
-export { ForgotPassword, Login, Register };
+export { ForgotPassword, Login, Register, ResetPassword };
