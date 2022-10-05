ebbe1fe7e55a10c43245562e7796c40b483200ba
maxwellwachira <maxwellwachira67@gmail.com>
2022-10-05 16:06:12 +0300

    added new imports

--- src/features/courses/index.ts
+++ src/features/courses/index.ts
@@ -4,9 +4,11 @@ import CourseTable from "./components/courseTable/courseTable";
 import PrintButton from "./components/reportButtons/printButton";
 import ExcelButton from "./components/reportButtons/excelButton";
 import PdfButton from "./components/reportButtons/pdfButton";
+import AddCourseModal from "./components/addCourseModal/addCourseModal";
 
 export {
     AddButton,
+    AddCourseModal,
     CategoriesTable,
     CourseTable,
     ExcelButton,
