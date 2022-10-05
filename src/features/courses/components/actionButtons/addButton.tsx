e6bc47b721372c137329cee515af19d0e112101f
maxwellwachira <maxwellwachira67@gmail.com>
2022-10-05 16:07:18 +0300

    added some polymorphism to add buton such that it can be used in both category and course

--- src/features/courses/components/actionButtons/addButton.tsx
+++ src/features/courses/components/actionButtons/addButton.tsx
@@ -1,26 +1,46 @@
+import { useState } from 'react';
 import { Text, Button } from "@mantine/core";
 import { IconPlus } from "@tabler/icons";
 
 import { useStyles } from './actionButtons.styles';
+import  AddCourseModal from '../addCourseModal/addCourseModal';
 
 interface UserID {
     id: number;
     type: string;
 }
 
+const selectData = [
+    { value: 'react', label: 'React' },
+    { value: 'ng', label: 'Angular' },
+    { value: 'svelte', label: 'Svelte' },
+    { value: 'vue', label: 'Vue' },
+];
+
+
 const AddButton = ({id, type}: UserID) => {
+    const [openAddCourse, setOpenAddCourse] = useState(false);
+    const [openAddCategory, setOpenAddCategory] = useState(false);
     const { classes } = useStyles();
+
     const onClick = () => {
-        console.log(id);
+        type === 'Course' ? setOpenAddCourse(true) : setOpenAddCategory(true);
+    } 
+    const onClose = () => {
+        type === 'Course' ? setOpenAddCourse(false) : setOpenAddCategory(false);
     }
     return (
-        <Button 
-            onClick={onClick} 
-            className={`${classes.button} ${classes.addButton}`}
-            leftIcon={<IconPlus />}
-        >
-            <Text size="xl">Add {type}</Text>
-        </Button>
+       <>
+            <Button 
+                onClick={onClick} 
+                className={`${classes.button} ${classes.addButton}`}
+                leftIcon={<IconPlus />}
+            >
+                <Text size="xl">Add {type}</Text>
+            </Button>
+            <AddCourseModal open={openAddCourse}  selectData={selectData} onClose={onClose}/>
+        
+        </>
     )
 }
 
