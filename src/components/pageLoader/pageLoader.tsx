da35af10de1a0914d8a7af150817c73ed12b97eb
maxwellwachira <maxwellwachira67@gmail.com>
2022-10-09 08:04:01 +0300

    removed css in js styling to css file

--- src/components/pageLoader/pageLoader.tsx
+++ src/components/pageLoader/pageLoader.tsx
@@ -1,13 +1,12 @@
-import { Box, Loader } from '@mantine/core';
+import { Box, Loader,  } from '@mantine/core';
 
-import { useStyles } from './pageLoader.styles';
 import { colors } from '../../constants/colors';
+import  styles from './PageLoader.module.css';
 
 const PageLoader = () => {
-    const { classes } = useStyles();
-    
+
     return (
-        <Box className={classes.wrapper}>
+        <Box className={styles["wrapper"]}>
             <Loader size="lg" variant="bars" color={colors.secondaryColor}/>
         </Box>
     );
