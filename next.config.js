41c7a4b0949860e15859ca50ef00dbbd7293a5b8
maxwellwachira <maxwellwachira67@gmail.com>
2022-10-10 05:37:40 +0300

    updated next config file to load image from remote url

--- next.config.js
+++ next.config.js
@@ -9,15 +9,12 @@ module.exports = {
 }
 
 module.exports = {
-  images: {
-    domains: ['mygrannyslove.com'],
-  }
-}
-
-module.exports = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  images: {
+    domains: ['mygrannyslove.com'],
+  }
 }
