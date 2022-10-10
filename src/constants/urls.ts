6e90535aeca424e74bc9a01e3dbed51fb692233e
maxwellwachira <maxwellwachira67@gmail.com>
2022-10-10 09:52:36 +0300

    added zoom api base url

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,7 +1,8 @@
 import { getCookie } from "cookies-next"
 export const urls = {
     baseUrl: 'http://localhost:5000',
-    tutorLms: 'https://mygrannyslove.com/wp-json/tutor/v1'
+    tutorLms: 'https://mygrannyslove.com/wp-json/tutor/v1',
+    zoomBaseUrl: 'https://api.zoom.us/v2'
 }
 
 export const authHeader = {
