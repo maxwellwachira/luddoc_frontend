9804d4dd1e4f0d95639958bc79dfcad60b86cece
maxwellwachira <maxwellwachira67@gmail.com>
2022-10-10 09:59:26 +0300

    working contact form

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,27 +1,50 @@
-import { Box, Button, Container, Divider, Grid, Group, Stack, Text, Textarea, TextInput } from '@mantine/core';
+import { useState } from 'react';
+import { Box, Button, Container, Divider, Grid, Group, Stack, Text, Textarea, TextInput, Notification } from '@mantine/core';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import { useForm } from '@mantine/form';
+import axios from 'axios';
+import { IconCheck, IconX } from '@tabler/icons';
 
 import MainLayout from '../layouts/mainLayout/mainLayout';
 import FooterLinks from '../components/footer/footer';
 import { footerData } from '../constants/footer';
 import { colors } from '../constants/colors';
-
+import { urls } from '../constants/urls';
 
 const Contact: NextPage = () => {
+    const [response, setResponse] = useState('')
+    const [loading, setLoading] = useState(false);
+    const  initialValues = {
+        email: '',
+        subject: '',
+        message: '',
+    };
 
     const form = useForm({
-        initialValues: {
-          email: '',
-          subject: '',
-          message: '',
-        },
+        initialValues,
         validate: {
-          email: (val) => /^\S+@\S+$/.test(val) && 'Invalid email',
+            email: (value) => (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value) ? null : 'Invalid email'),
         },
     });
+
+    const handleSubmit = async() => {
+        setLoading(true);
+        if(JSON.stringify(form.errors) === "{}"){
+            try {
+                const { data } = await axios.post(`${urls.baseUrl}/email/send-contact`, form.values);
+                if(data.message === 'success') {
+                    setResponse(data.message);
+                    setLoading(false);
+                    form.setValues(initialValues);
+                }
+            } catch (error: any) {
+               console.log(error);
+               setResponse('error')
+            }
+        }
+    }
 
   return (
     <>
@@ -46,8 +69,16 @@ const Contact: NextPage = () => {
                     </Text>
 
                     <Divider label="Talk to us" labelPosition="center" my="lg" />
-
-                    <form onSubmit={form.onSubmit(() => {})}>
+                    {response === 'success' ? (
+                        <Notification icon={<IconCheck size={18} />} color="teal" title="Success" onClose={() => setResponse('')}>
+                            We have received your Message, we will get back to you shortly
+                        </Notification>
+                    ): response ? (
+                        <Notification icon={<IconX size={18} />} color="red" title="Error" onClose={() => setResponse('')}>
+                            There was an error in sending the message, Try again later
+                        </Notification>
+                    ): ''}
+                    <form onSubmit={form.onSubmit(() => {handleSubmit()})}>
                         <Stack>
                         
                             <TextInput
@@ -63,7 +94,7 @@ const Contact: NextPage = () => {
                                 required
                                 label="Subject"
                                 placeholder="How to enroll"
-                                value={form.values.email}
+                                value={form.values.subject}
                                 onChange={(event) => form.setFieldValue('subject', event.currentTarget.value)}
                             />
 
@@ -78,7 +109,7 @@ const Contact: NextPage = () => {
                         </Stack>
 
                         <Group position="apart" mt="xl">
-                            <Button type="submit" style={{background: `${colors.secondaryColor}`}}>Send</Button>
+                            <Button type="submit" style={{background: `${colors.secondaryColor}`}} loading={loading}>Send</Button>
                         </Group>
                     </form>
                 </Grid.Col>
