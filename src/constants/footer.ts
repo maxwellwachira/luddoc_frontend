7e1636d394a0963c0f8009e50ac7765697652c54
maxwellwachira <maxwellwachira67@gmail.com>
2022-10-11 09:07:06 +0300

    replaced dummy links with actual links

--- src/constants/footer.ts
+++ src/constants/footer.ts
@@ -2,37 +2,37 @@ export const footerData = [
     {
         title: 'Site Map', 
         links: [
-            { id: 0, link: 'https://xx.com', label: 'Home' },
-            { id: 1, link: 'https://xx.com', label: 'About' },
-            { id: 2, link: 'https://xx.com', label: 'Courses' },
-            { id: 3, link: 'https://xx.com', label: 'FAQ' }
+            { id: 0, link: '/', label: 'Home' },
+            { id: 1, link: '/about', label: 'About' },
+            { id: 2, link: '/courses', label: 'Courses' },
+            { id: 3, link: '/faq', label: 'FAQ' }
         ]
     },
     {
         title: 'Courses', 
         links: [
-            { id: 0, link: 'https://xx.com', label: 'Taking Care of Elderly' },
-            { id: 1, link: 'https://xx.com', label: 'First Aid' },
-            { id: 2, link: 'https://xx.com', label: 'Self Development' },
-            { id: 3, link: 'https://xx.com', label: 'Lending' }
+            { id: 0, link: '/courses/59260', label: 'Dementia Care' },
+            { id: 1, link: '/courses/58949', label: 'First Aid' },
+            { id: 2, link: '/courses/59655', label: 'Emotional Intelligence' },
+            { id: 3, link: '/courses/60199', label: 'Mindful Meditation' }
         ]
     },
     {
         title: 'Contact', 
         links: [
-            { id: 0, link: 'https://xx.com', label: 'Contact Page' },
-            { id: 1, link: 'https://xx.com', label: 'Support' },
-            { id: 2, link: 'https://xx.com', label: 'Info' },
-            { id: 2, link: 'https://xx.com', label: 'Sales' },
+            { id: 0, link: '/contact', label: 'Contact Page' },
+            { id: 1, link: 'mailto:luddoc.institute@gmail.com', label: 'Support' },
+            { id: 2, link: 'mailto:luddoc.institute@gmail.com', label: 'Info' },
+            { id: 2, link: 'mailto:luddoc.institute@gmail.com', label: 'Sales' },
         ]
     }, 
     {
-        title: 'Social Links', 
+        title: 'Social Links',
         links: [
-            { id: 0, link: 'https://xx.com', label: 'Twitter' },
-            { id: 1, link: 'https://xx.com', label: 'LinkedIn' },
-            { id: 2, link: 'https://xx.com', label: 'Instagram' },
-            { id: 3, link: 'https://xx.com', label: 'Facebook' }
+            { id: 0, link: '', label: 'Twitter' },
+            { id: 1, link: '', label: 'LinkedIn' },
+            { id: 2, link: '', label: 'Instagram' },
+            { id: 3, link: '', label: 'Facebook' }
         ]
     },
 ];
